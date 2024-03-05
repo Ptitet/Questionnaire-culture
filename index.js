353b3a6c68f2b38d7a6eaf897d9ba025beb9e787
le.michel <ptitet73@gmail.com>
2024-03-05 22:18:26 +0100

    ajout de questions de test pour visualiser les layouts, la generation du html fonctionne

--- index.js
+++ index.js
@@ -13,12 +13,40 @@ const questions = [
     {
         question: 'Quel est le plus grand pays du monde ?',
         responseType: 2,
+        goodAnswer: 'Russie',
         answers: [
             'Russie',
             'Canada',
             'Chine',
             'USA'
         ]
+    },
+    {
+        question: 'Quels sont les bonnes réponses ?',
+        responseType: 1,
+        goodAnswer: ['Celle-ci', 'Celle-ci aussi', 'Celle-là'],
+        answers: [
+            'Celle-là',
+            'Celle-ci',
+            'Mais pas celle-là',
+            'Celle-ci aussi'
+        ]
+    },
+    {
+        question: 'Quels sont les 3 premiers chiffres de la suite de Fibonacci ?',
+        responseType: 0,
+        goodAnswer: '0, 1, 1',
+        answers: [
+            '0, 1, 2',
+            '0, 1, 1',
+            '1, 1, 2',
+            '1, 2, 3'
+        ]
+    },
+    {
+        question: 'Quel est le nombre de jours dans une année bissextile ?',
+        responseType: 3,
+        goodAnswer: 366
     }
 ];
 
@@ -43,12 +71,11 @@ for (const [i, question] of Object.entries(questions)) {
     const answersContainer = document.createElement('div');
     answersContainer.classList.add('answers');
 
-    for (let answer of question.answers) {
-        let answerElement = document.createElement('div');
-        answerElement.classList.add('answer');
-
-        switch(question.responseType) {
-            case 0: {
+    switch (question.responseType) {
+        case 0: {
+            for (let answer of question.answers) {
+                let answerElement = document.createElement('div');
+                answerElement.classList.add('answer');
                 const input = document.createElement('input');
                 input.type = 'radio';
                 input.name = `q${qn}`;
@@ -58,17 +85,50 @@ for (const [i, question] of Object.entries(questions)) {
                 label.innerText = answer;
                 label.htmlFor = input.id;
                 answerElement.appendChild(label);
-                break;
+                answersContainer.appendChild(answerElement);
             }
-            case 1: {
-                break;
-            }
-            case 2: {
-                // select menu
-            }
+            break;
         }
-
-        answersContainer.appendChild(answerElement);
+        case 1: {
+            for (let answer of question.answers) {
+                let answerElement = document.createElement('div');
+                answerElement.classList.add('answer');
+                const input = document.createElement('input');
+                input.type = 'checkbox';
+                input.name = `q${qn}`;
+                input.id = `q${qn}-${answer}`;
+                answerElement.appendChild(input);
+                const label = document.createElement('label');
+                label.innerText = answer;
+                label.htmlFor = input.id;
+                answerElement.appendChild(label);
+                answersContainer.appendChild(answerElement);
+            }
+            break;
+        }
+        case 2: {
+            const answerElement = document.createElement('div');
+            const select = document.createElement('select');
+            select.name = `q${qn}`;
+            for (let answer of question.answers) {
+                const option = document.createElement('option');
+                option.value = answer;
+                option.innerText = answer;
+                select.appendChild(option);
+            }
+            answerElement.appendChild(select);
+            answersContainer.appendChild(answerElement);
+            break;
+        }
+        case 3: {
+            const answerElement = document.createElement('div');
+            const input = document.createElement('input');
+            input.type = 'number';
+            input.name = `q${qn}`;
+            answerElement.appendChild(input);
+            answersContainer.appendChild(answerElement);
+            break;
+        }
     }
 
     qWrapper.appendChild(answersContainer);
