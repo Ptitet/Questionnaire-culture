d14c61bb63746c67188d76f0727a0465b2083ad9
le.michel <ptitet73@gmail.com>
2024-03-06 21:28:39 +0100

    refactor et split du js en différents fichier dans un dossier js + questions dorénavant dans questions.js

---
+++ js/questions.js
@@ -0,0 +1,51 @@
+export const questions = [
+    {
+        question: 'Dans quel sport ne joue-t-on pas avec une balle ?',
+        responseType: 0, // 0: radio | 1: checkbox | 2: select | 3: number
+        goodAnswer: 'Badminton',
+        answers: [
+            'Basket',
+            'Badminton',
+            'Golf',
+            'Voleyball'
+        ]
+    },
+    {
+        question: 'Quel est le plus grand pays du monde ?',
+        responseType: 2,
+        goodAnswer: 'Russie',
+        answers: [
+            'Russie',
+            'Canada',
+            'Chine',
+            'USA'
+        ]
+    },
+    {
+        question: 'Quels sont les bonnes réponses ?',
+        responseType: 1,
+        goodAnswer: ['Celle-ci', 'Celle-ci aussi', 'Celle-là'],
+        answers: [
+            'Celle-là',
+            'Celle-ci',
+            'Mais pas celle-là',
+            'Celle-ci aussi'
+        ]
+    },
+    {
+        question: 'Quels sont les 3 premiers chiffres de la suite de Fibonacci ?',
+        responseType: 0,
+        goodAnswer: '0, 1, 1',
+        answers: [
+            '0, 1, 2',
+            '0, 1, 1',
+            '1, 1, 2',
+            '1, 2, 3'
+        ]
+    },
+    {
+        question: 'Quel est le nombre de jours dans une année bissextile ?',
+        responseType: 3,
+        goodAnswer: '366'
+    }
+];
